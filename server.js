687fe3dd5bbd6e78659fe811f143a11194c95bce
Alextricity <alexander.sell@hotmail.com>
2018-07-05 11:05:34 +0100

    added routes and bcrypt

--- server.js
+++ server.js
@@ -12,18 +12,64 @@ app.get('/', (req,res) => {
 	res.send('test');
 })
 
-app.get('/user/:id' , (req,res) => {
-	const {id} = req.params;
-	let userFound = false;
-	db.users.forEach(user => {
-		if(user.id === id){
-			userFound = true;
-			return res.json(user);
+app.post('/signin', (req, res) => {
+	db.select('email', 'hash').from('login')
+	.where('email', '=', req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if (isValid) {
+			return db.select('*').from('users')
+			.where('email', '=', req.body.email)
+			.then(user => {
+				res.json(user[0])
+			})
+			.catch(err => res.status(400).json('unable to get user'))
+		} else {
+			res.status(400).json('wrong credentials')
 		}
 	})
-	if(!userFound){
-		res.status(404).json('no user found')
-	}
+	.catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	const hash = bcrypt.hashSync(password);
+	db.transaction(compare => {
+		compare.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return compare('users')
+			.returning('*')
+			.insert({
+				email: loginEmail[0],
+				name: name,
+				joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(compare.commit)
+		.catch(compare.rollback)
+	})
+	.catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/user/:id', (req, res) => {
+	const { id } = req.params;
+	db.select('*').from('users').where({id})
+	.then(user => {
+		if (user.length) {
+			res.json(user[0])
+		} else {
+			res.status(400).json('Not found')
+		}
+	})
+	.catch(err => res.status(400).json('error getting user'))
 })
 
 app.post('/image', (req,res) => {
