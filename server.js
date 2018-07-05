1fecbb237e93e5f78fca457278450e9071f27b07
Alextricity <alexander.sell@hotmail.com>
2018-07-05 10:58:29 +0100

    added packages

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors')
+
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.get('/', (req,res) => {
 	res.send('test');
+})
+
+app.get('/user/:id' , (req,res) => {
+	const {id} = req.params;
+	let userFound = false;
+	db.users.forEach(user => {
+		if(user.id === id){
+			userFound = true;
+			return res.json(user);
+		}
+	})
+	if(!userFound){
+		res.status(404).json('no user found')
+	}
+})
+
+app.post('/image', (req,res) => {
+
 })
 
 app.listen(3000, () => {
